dff2bb5978b27b037d20a18bb1b4f9041af9489b
martin.hoch <your_email_id>
2023-07-24 13:08:35 +0200

    edit Intro

--- Intro.py
+++ Intro.py
@@ -6,6 +6,11 @@ st.markdown(
         - On *Certificates, Languages, Skills and Users* tabs you can view data and add new or assign them to users
         - On the *Overview* tab you can view and filter all data combined
         - To remove user and all his assigned abbilities, go to *Remove user* tab
+
+        ### First steps
+        - Go to *Users* tab and input your name and contact information to the db
+        - Assign skill/certificates/languages to your name
+        - if the skill/certfificate or language is not in options, add it
         """
         )
 
